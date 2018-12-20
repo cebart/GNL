da26da23352f6cc2f48be78290ec5fa579cca1cf
cebart <33933281+cebart@users.noreply.github.com>
2018-12-20 02:29:19 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -38,7 +38,7 @@ static char		*ft_put_line(char *str)
 
 int			get_next_line(const int fd, char **line)
 {
-	static t_file	files[100]; /* OPEN_MAX*/
+	static char	    *files[100]; /* OPEN_MAX*/
 	int				ret;
 	char			*str;
 
@@ -46,21 +46,19 @@ int			get_next_line(const int fd, char **line)
 	str = ft_strnew(BUFF_SIZE + 1);
 	if (fd < 0 || BUFF_SIZE < 0 || !line)
 		return (-1);
-	if (!files[fd].buf)
-		files[fd].buf = ft_strnew(BUFF_SIZE + 1);
-	while (!(ft_strchr(files[fd].buf, '\n')))
+	if (!files[fd])
+		files[fd] = ft_strnew(BUFF_SIZE + 1);
+	while (!(ft_strchr(files[fd], '\n')))
 	{
 		ret = read(fd, str, BUFF_SIZE);
 		if (ret == -1)
-			return (-1);
-		str[ret] = '\0';
-		files[fd].buf = ft_strjoin(files[fd].buf, str);
-		if (ret == 0 && files[fd].buf[0] == '\0')
 			return (0);
+		str[ret] = '\0';
+		files[fd] = ft_strjoin(files[fd], str);
 		if (ret == 0)
-			break ;
+			return (0);
 	}
-	*line = ft_put_line(files[fd].buf);
-	files[fd].buf = ft_too_much_read(files[fd].buf);
+	*line = ft_put_line(files[fd]);
+	files[fd] = ft_too_much_read(files[fd]);
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -2,12 +2,8 @@
 # define GET_NEXT_LINE_H
 # include "libft/libft.h"
 # include "get_next_line.h"
-# define BUFF_SIZE 22
+# define BUFF_SIZE 1
 
-typedef struct	s_file
-{
-	char	*buf;
-}				t_file;
 int	get_next_line(const int fd, char **line);
 
 #endif
